afef501a3d0bfdff07c648fc50cfea6678b1aafd
JpgDev <samajpablo@gmail.com>
2024-04-15 17:25:07 -0600

    feat: add multilanguage routes

---
+++ src/i18n/ui.ts
@@ -0,0 +1,22 @@
+export const languages = {
+    en: 'English',
+    es: 'Español',
+  };
+
+  export const showDefaultLang = false;
+  export const defaultLang = 'es';
+  
+  export const ui = {
+    es: {
+        'subtitle': "Ingeniero",
+        'nav.home': 'Inicio',
+        'nav.projects': 'Proyectos',
+        'nav.contact' : 'Contacto'
+    },
+    en: {
+        'subtitle': "Developer",
+        'nav.home': 'Home',
+        'nav.projects': 'Projects',
+        'nav.contact' : 'Contact me '
+    },
+  } as const;
