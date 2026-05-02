727155062b1920188bf5f589dd7a035ec7ad48d7
juanpsama <samajpablo@gmail.com>
2026-05-02 10:03:01 -0600

    feat: extract IndexPage on a component to simplify traslations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,13 +8,23 @@ export const languages = {
   
   export const ui = {
     es: {
-        'subtitle': "Desarrollador de software",
+        "title": "Juan P. | Inicio",
+        "subtitle": "Desarrollador de software",
+        "experience" : "Experiencia",
+        "aboutMe":"Sobre mí",
+        "present": "Actualidad",
+
         'nav.home': 'Inicio',
         'nav.projects': 'Proyectos',
         'nav.contact' : 'Contacto'
     },
     en: {
-        'subtitle': "Software Developer",
+        "title": "Juan P. | About",
+        "subtitle": "Software Developer",
+        "experience" : "Experience",
+        "aboutMe":"About me",
+        "present": "Present",
+
         'nav.home': 'About me',
         'nav.projects': 'Projects',
         'nav.contact' : 'Contact me '
