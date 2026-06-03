a7f392e28d1a138c0c5fb510ba0c57c4a613bdca
jp <samajpablo@gmail.com>
2026-06-03 10:52:09 -0600

    feat: add preact library for dynamic islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind";
 
 import icon from "astro-icon";
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), preact()],
   site: 'https://juanpsama.github.io',
   base: '/portfolio-astro',
   i18n: {
