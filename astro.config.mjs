9c49e1c91dd0fed71d935897dd1584bf9804e188
jp <samajpablo@gmail.com>
2026-06-03 12:39:48 -0600

    feat: update tailwind version and fix  compatibility with outdated clases to maintain the style across the pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,26 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+// import tailwind from "@astrojs/tailwind";
 
 import icon from "astro-icon";
 
 import preact from '@astrojs/preact';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), preact()],
+  integrations: [icon(), preact()],
   site: 'https://juanpsama.github.io',
   base: '/portfolio-astro',
+
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
     prefixDefaultLocale: true,
     redirectToDefaultLocale: false,
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
